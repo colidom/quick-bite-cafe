906d43b4ee9a966a5caaff54716e557cfa8a5263
colidom <colidom@outlook.com>
2024-04-09 00:04:14 +0200

    Primeros Pasos con el Formulario de Registro

--- front/src/views/Register.jsx
+++ front/src/views/Register.jsx
@@ -1,3 +1,23 @@
 export default function Register() {
-    return <div>Register</div>;
+    return (
+        <>
+            <h1 className="text-4xl front-black">Crea tu cuenta</h1>
+            <p>Introduce tus datos para crear una cuenta</p>
+
+            <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
+                <form action="">
+                    <div className="mb-4">
+                        <label className="text-slate-800" htmlFor="name">
+                            Nombre:{" "}
+                        </label>
+                        <input type="text" 
+                        id="name" 
+                        className="mt-2 blck p-3 bg-gray-100" 
+                        name="name" 
+                        placeholder="Tu nombre" />
+                    </div>
+                </form>
+            </div>
+        </>
+    );
 }
