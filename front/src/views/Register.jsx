d0a7a6b057cde5024a1d0e65a679d1fdc13b62b8
colidom <colidom@outlook.com>
2024-04-09 00:25:45 +0200

    Formateo vista Register

--- front/src/views/Register.jsx
+++ front/src/views/Register.jsx
@@ -12,43 +12,30 @@ export default function Register() {
                         <label className="text-slate-800" htmlFor="name">
                             Nombre:
                         </label>
-                        <input type="text" 
-                            id="name" 
-                            className="mt-2 w-full p-3 bg-gray-100" 
-                            name="name" 
-                            placeholder="Tu nombre" />
+                        <input type="text" id="name" className="mt-2 w-full p-3 bg-gray-100" name="name" placeholder="Tu nombre" />
                     </div>
                     <div className="mb-4">
                         <label className="text-slate-800" htmlFor="surname">
                             Apellido:
                         </label>
-                        <input type="text" 
-                            id="surname" 
-                            className="mt-2 w-full p-3 bg-gray-100" 
-                            name="surname" 
-                            placeholder="Tu apellido" />
+                        <input type="text" id="surname" className="mt-2 w-full p-3 bg-gray-100" name="surname" placeholder="Tu apellido" />
                     </div>
                     <div className="mb-4">
                         <label className="text-slate-800" htmlFor="email">
                             Email:
                         </label>
-                        <input type="email" 
-                            id="email" 
-                            className="mt-2 w-full p-3 bg-gray-100" 
-                            name="email" 
-                            placeholder="Tu email" 
-                            formNoValidate
-                        />
+                        <input type="email" id="email" className="mt-2 w-full p-3 bg-gray-100" name="email" placeholder="Tu email" formNoValidate />
                     </div>
                     <div className="mb-4">
                         <label className="text-slate-800" htmlFor="email">
                             Contraseña:
                         </label>
-                        <input type="password" 
-                            id="password" 
-                            className="mt-2 w-full p-3 bg-gray-100" 
-                            name="password" 
-                            placeholder="Tu contraseña" 
+                        <input
+                            type="password"
+                            id="password"
+                            className="mt-2 w-full p-3 bg-gray-100"
+                            name="password"
+                            placeholder="Tu contraseña"
                             formNoValidate
                         />
                     </div>
@@ -56,24 +43,24 @@ export default function Register() {
                         <label className="text-slate-800" htmlFor="email">
                             Repetir contraseña:
                         </label>
-                        <input type="password" 
-                            id="password_confirmation" 
-                            className="mt-2 w-full p-3 bg-gray-100" 
-                            name="password_confirmation" 
-                            placeholder="Repite tu contraseña" 
+                        <input
+                            type="password"
+                            id="password_confirmation"
+                            className="mt-2 w-full p-3 bg-gray-100"
+                            name="password_confirmation"
+                            placeholder="Repite tu contraseña"
                             formNoValidate
                         />
                     </div>
-                    <input type="submit" 
+                    <input
+                        type="submit"
                         value="Crear cuenta"
                         className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
                     />
                 </form>
             </div>
             <nav className="mt-5">
-                <Link to="/auth/login">
-                    ¿Ya tienes una cuenta? ¡Iniciar sesión!
-                </Link>
+                <Link to="/auth/login">¿Ya tienes una cuenta? ¡Iniciar sesión!</Link>
             </nav>
         </>
     );
