ae493f770b18f0b78ad78c18b722a5a54d1b77a7
colidom <colidom@outlook.com>
2024-04-09 00:18:20 +0200

    Creando la Página y Formulario de Autenticación

--- front/src/views/Login.jsx
+++ front/src/views/Login.jsx
@@ -1,3 +1,41 @@
 export default function Login() {
-    return <div>Login</div>;
+    return (
+        <>
+            <h1 className="text-4xl front-black">Iniciar sesión</h1>
+            <p>Antes de crear un pedido, primero inicia sesión con tu cuenta</p>
+
+            <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
+                <form action="">
+                    <div className="mb-4">
+                        <label className="text-slate-800" htmlFor="email">
+                            Email:
+                        </label>
+                        <input type="email" 
+                            id="email" 
+                            className="mt-2 w-full p-3 bg-gray-100" 
+                            name="email" 
+                            placeholder="Tu email" 
+                            formNoValidate
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="text-slate-800" htmlFor="email">
+                            Contraseña:
+                        </label>
+                        <input type="password" 
+                            id="password" 
+                            className="mt-2 w-full p-3 bg-gray-100" 
+                            name="password" 
+                            placeholder="Tu contraseña" 
+                            formNoValidate
+                        />
+                    </div>
+                    <input type="submit" 
+                        value="Iniciar sesión"
+                        className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
+                    />
+                </form>
+            </div>
+        </>
+    );
 }
