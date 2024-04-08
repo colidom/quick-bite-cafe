a4b7347f36a225d99cc5a9e831743b38bc164080
colidom <colidom@outlook.com>
2024-04-09 00:25:04 +0200

    Formateo vista Login

--- front/src/views/Login.jsx
+++ front/src/views/Login.jsx
@@ -12,36 +12,30 @@ export default function Login() {
                         <label className="text-slate-800" htmlFor="email">
                             Email:
                         </label>
-                        <input type="email" 
-                            id="email" 
-                            className="mt-2 w-full p-3 bg-gray-100" 
-                            name="email" 
-                            placeholder="Tu email" 
-                            formNoValidate
-                        />
+                        <input type="email" id="email" className="mt-2 w-full p-3 bg-gray-100" name="email" placeholder="Tu email" formNoValidate />
                     </div>
                     <div className="mb-4">
                         <label className="text-slate-800" htmlFor="email">
                             Contraseña:
                         </label>
-                        <input type="password" 
-                            id="password" 
-                            className="mt-2 w-full p-3 bg-gray-100" 
-                            name="password" 
-                            placeholder="Tu contraseña" 
+                        <input
+                            type="password"
+                            id="password"
+                            className="mt-2 w-full p-3 bg-gray-100"
+                            name="password"
+                            placeholder="Tu contraseña"
                             formNoValidate
                         />
                     </div>
-                    <input type="submit" 
+                    <input
+                        type="submit"
                         value="Iniciar sesión"
                         className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
                     />
                 </form>
             </div>
             <nav className="mt-5">
-                <Link to="/auth/register">
-                    ¿Aún no tienes una cuenta? ¡Crea una, es gratis!
-                </Link>
+                <Link to="/auth/register">¿Aún no tienes una cuenta? ¡Crea una, es gratis!</Link>
             </nav>
         </>
     );
