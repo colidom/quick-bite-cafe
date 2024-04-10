acf06cc95da0b016978ce7d0e7a29f508393e707
colidom <colidom@outlook.com>
2024-04-10 12:50:29 +0200

    Creando el Layout Principal

---
+++ app-front/src/components/Summery.jsx
@@ -0,0 +1,3 @@
+export default function Summery() {
+    return <div className="md:w-72">Resumen</div>;
+}
--- app-front/src/layouts/Layout.jsx
+++ app-front/src/layouts/Layout.jsx
@@ -1,10 +1,17 @@
 import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import Summery from "../components/Summery";
 
 export default function Layout() {
     return (
-        <div>
-            Layout
-            <Outlet />
+        <div className="md:flex">
+            <Sidebar />
+
+            <main className="flex-1">
+                <Outlet />
+            </main>
+
+            <Summery />
         </div>
     );
 }
