bd94ea278dde95033dd675944a8ea697f4be0cde
colidom <colidom@outlook.com>
2024-04-11 14:04:51 +0200

    Mostrando Imagen, Precio y Nombre del Producto

--- app-front/src/components/Product.jsx
+++ app-front/src/components/Product.jsx
@@ -1,4 +1,13 @@
 export default function Product({ product }) {
-    console.log(product);
-    return <div>Producto</div>;
+    const { name, image, price } = product;
+
+    return (
+        <div className="border p-3 shaow bg-white">
+            <img src={`/img/${image}.jpg`} alt={`Imagen ${name}`} />
+            <div className="py-5">
+                <h3 className="text-2xl font-bold">{name}</h3>
+                <p className="mt-5 font-black text-4xl text-amber-500">{price}</p>
+            </div>
+        </div>
+    );
 }
