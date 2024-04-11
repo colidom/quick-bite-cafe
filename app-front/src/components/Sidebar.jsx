a01fc4aa766a4ccb126ea4ea5303e20c66854752
colidom <colidom@outlook.com>
2024-04-11 12:10:53 +0200

    Obteniendo y Mostrando las Categorías

--- app-front/src/components/Sidebar.jsx
+++ app-front/src/components/Sidebar.jsx
@@ -1,3 +1,17 @@
+import { categories } from "../data/categories";
+
 export default function Sidebar() {
-    return <div className="md:w-72">Sidebar</div>;
+    return (
+        <aside className="md:w-72">
+            <div className="p-4">
+                <img src="img/logo.svg" alt="imagen logotipo" className="w-40" />
+            </div>
+
+            <div className="mt-10">
+                {categories.map((category) => (
+                    <p> {category.name}</p>
+                ))}
+            </div>
+        </aside>
+    );
 }
---
+++ app-front/src/data/categories.js
@@ -0,0 +1,49 @@
+const categories = [
+    {
+        icono: "cafe",
+        name: "Café",
+        id: 1,
+    },
+    {
+        icono: "hamburguesa",
+        name: "Hamburguesas",
+        id: 2,
+    },
+    {
+        icono: "pizza",
+        name: "Pizzas",
+        id: 3,
+    },
+    {
+        icono: "donuts",
+        name: "Donuts",
+        id: 4,
+    },
+    {
+        icono: "pastel",
+        name: "Pasteles",
+        id: 5,
+    },
+    {
+        icono: "galletas",
+        name: "Galletas",
+        id: 6,
+    },
+    {
+        icono: "helado",
+        name: "Helados",
+        id: 7,
+    },
+    {
+        icono: "sushi",
+        name: "Sushi",
+        id: 8,
+    },
+    {
+        icono: "ensalada",
+        name: "Ensaladas",
+        id: 9,
+    },
+];
+
+export { categories };
