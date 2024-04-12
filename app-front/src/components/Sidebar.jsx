bb3409e2bcb0ea0376a67e8900097a13d4c5e126
colidom <colidom@outlook.com>
2024-04-12 23:58:26 +0200

    Formateo

--- app-front/src/components/Sidebar.jsx
+++ app-front/src/components/Sidebar.jsx
@@ -10,9 +10,7 @@ export default function Sidebar() {
 
             <div className="mt-10">
                 {categories.map((category) => (
-                    <Category 
-                        key={category.id} 
-                        category={category} />
+                    <Category key={category.id} category={category} />
                 ))}
             </div>
 
