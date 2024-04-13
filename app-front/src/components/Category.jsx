8d810e43f9781b2f3dbb3606e57f632e2037561c
colidom <colidom@outlook.com>
2024-04-13 15:43:15 +0200

    Resaltando la Categoría Actual

--- app-front/src/components/Category.jsx
+++ app-front/src/components/Category.jsx
@@ -2,11 +2,13 @@ import PropTypes from "prop-types";
 import useKiosk from "../hooks/useKiosk";
 
 export default function Category({ category }) {
-    const { handleClickCategory } = useKiosk();
+    const { handleClickCategory, currentCategory } = useKiosk();
     const { icono, id, name } = category;
 
+    const highlightCurrentCategory = () => (currentCategory.id === id ? "bg-amber-400" : "bg-white");
+
     return (
-        <div className="flex items-center gap-4 border w-full py-3 hover:bg-amber-400 cursor-pointer">
+        <div className={`${highlightCurrentCategory()} flex items-center gap-4 border w-full py-3 hover:bg-amber-400 cursor-pointer`}>
             <img src={`/img/icono_${icono}.svg`} alt="Imagen icono" className="w-12" />
             <button className="text-lg font-bold cursor-pointer truncate" type="button" onClick={() => handleClickCategory(id)}>
                 {name}
