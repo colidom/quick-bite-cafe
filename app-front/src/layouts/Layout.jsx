4433e7096404362cce9e55ae6a774cb1f41bba90
colidom <colidom@outlook.com>
2024-04-15 23:33:01 +0200

    Instalando y Configurando React Modal

--- app-front/src/layouts/Layout.jsx
+++ app-front/src/layouts/Layout.jsx
@@ -1,22 +1,42 @@
 import { Outlet } from "react-router-dom";
+import Modal from "react-modal";
 import Sidebar from "../components/Sidebar";
 import Summery from "../components/Summery";
 import useKiosk from "../hooks/useKiosk";
 
+const customStyles = {
+    content: {
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        bottom: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)",
+    },
+};
+
 export default function Layout() {
-    const { modal } = useKiosk();
+    const { modal, handleClickModal } = useKiosk();
 
     console.log(modal);
 
     return (
-        <div className="md:flex">
-            <Sidebar />
+        <>
+            <div className="md:flex">
+                <Sidebar />
 
-            <main className="flex-1 h-screen overflow-y-scroll bg-gray-100">
-                <Outlet />
-            </main>
+                <main className="flex-1 h-screen overflow-y-scroll bg-gray-100">
+                    <Outlet />
+                </main>
 
-            <Summery />
-        </div>
+                <Summery />
+            </div>
+            {modal && (
+                <Modal isOpen={modal} style={customStyles}>
+                    <p>Desde Modal</p>
+                    <button onClick={handleClickModal}>Cerrar</button>
+                </Modal>
+            )}
+        </>
     );
 }
