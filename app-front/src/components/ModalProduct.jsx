7e5aa4fa6d37482046ff152b92d08da5e43d272b
colidom <colidom@outlook.com>
2024-04-17 00:30:07 +0200

    Añadiendo la funcionalidad de ajustar las cantidades

--- app-front/src/components/ModalProduct.jsx
+++ app-front/src/components/ModalProduct.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import useKiosk from "../hooks/useKiosk";
 import { formatCurrency } from "../helpers";
 
 export default function ModalProduct() {
     const { product, handleClickModal } = useKiosk();
+    const [qty, setQty] = useState(1);
     return (
         <div className="md:flex gap-10">
             <div className="md:w-1/3">
@@ -33,6 +35,46 @@ export default function ModalProduct() {
                 </p>
                 <hr className="my-5" />
                 <ul className="flex flex-wrap items-center gap-x-5"></ul>
+
+                <div className="flex gap-4 mt-5">
+                    <button
+                        type="button"
+                        onClick={() => {
+                            if (qty <= 1) return;
+                            setQty(qty - 1);
+                        }}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                        >
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                    </button>
+                    <div className="text-3xl">{qty}</div>
+                    <button
+                        type="button"
+                        onClick={() => {
+                            if (qty >= 5) return;
+                            setQty(qty + 1);
+                        }}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                        >
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                    </button>
+                </div>
                 <button type="button" className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 mt-5 text-white font-bold uppercase rounded">
                     Añadir al pedido
                 </button>
