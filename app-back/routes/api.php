decca75fc5ce59753b559c516e04570949d15c97
colidom <colidom@outlook.com>
2024-05-11 00:55:35 +0200

    Corregido problema al devolver el array de datos

--- app-back/routes/api.php
+++ app-back/routes/api.php
@@ -9,4 +9,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('/categories', [CategoryController::class]);
+Route::get('/categories', [CategoryController::class, 'index']);
