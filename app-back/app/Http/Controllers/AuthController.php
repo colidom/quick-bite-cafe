8f9938b0915e138e71421b7b53ac660e02615f5b
colidom <colidom@outlook.com>
2024-05-13 23:50:18 +0200

    Creando un Request de Autenticación

--- app-back/app/Http/Controllers/AuthController.php
+++ app-back/app/Http/Controllers/AuthController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
+        // Validar el registro
+        $data = $request->validated();
     }
 
     public function login(Request $request)
