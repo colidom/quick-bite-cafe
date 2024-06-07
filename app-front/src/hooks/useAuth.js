5049479d94fba56dc7ad49f1407bb4e0f3a92b72
colidom <colidom@outlook.com>
2024-06-07 23:31:34 +0200

    Revalidando el usuario autenticado con useSWR

--- app-front/src/hooks/useAuth.js
+++ app-front/src/hooks/useAuth.js
@@ -1,11 +1,32 @@
+import useSWR from "swr";
 import axiosClient from "../config/axios";
 
 export const useAuth = ({ middleware, url }) => {
+    // Obtenemos token del local storage
+    const token = localStorage.getItem("AUTH_TOKEN");
+
+    const {
+        data: user,
+        error,
+        mutate,
+    } = useSWR("/api/user", () =>
+        axiosClient("/api/user", {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then((res) => res.data)
+            .catch((error) => {
+                throw Error(error?.response?.data?.errors);
+            })
+    );
+
     const login = async (formData, setErrors) => {
         try {
             const { data } = await axiosClient.post("/api/login", formData);
             localStorage.setItem("AUTH_TOKEN", data.token);
             setErrors([]);
+            await mutate();
         } catch (error) {
             setErrors(Object.values(error.response.data.errors));
         }
@@ -14,6 +35,8 @@ export const useAuth = ({ middleware, url }) => {
     const register = () => {};
 
     const logout = () => {};
+
+    console.log(user, error);
 
     return {
         login,
