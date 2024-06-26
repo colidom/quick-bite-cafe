46f20b809f30bfe4f74c74b9783a29cc35a10fdf
colidom <colidom@outlook.com>
2024-06-27 01:35:10 +0200

    Finalizando de Mostrar el Contenido de la Orden

--- app-front/src/views/Orders.jsx
+++ app-front/src/views/Orders.jsx
@@ -1,5 +1,6 @@
 import useSWR from "swr";
 import axiosClient from "../config/axios";
+import { formatCurrency } from "../helpers";
 
 export default function Orders() {
     const token = localStorage.getItem("AUTH_TOKEN");
@@ -10,7 +11,7 @@ export default function Orders() {
             },
         });
 
-    const { data, error, isLoading } = useSWR("/api/order", fetcher);
+    const { data, isLoading } = useSWR("/api/order", fetcher);
 
     if (isLoading) return "Cargando pedidos...";
 
@@ -20,7 +21,7 @@ export default function Orders() {
                 <h1 className="text-4xl font-black">Pedidos</h1>
                 <p className="text-2xl my-10">Desde aquí puedes administrar los pedidos.</p>
             </div>
-            <div>
+            <div className="grid grid-cols-2 gap-5">
                 {data.data.data.map((order) => (
                     <div key={order.id} className="p-5 bg-white shadow space-y-2 border-b">
                         <p className="text-xl font-bold text-slate-600">Contenido del pedido:</p>
@@ -34,6 +35,22 @@ export default function Orders() {
                                 </p>
                             </div>
                         ))}
+                        <p className="text-lg font-bold text-slate-600">
+                            Cliente: {""}
+                            <span className="font-normal">
+                                {order.user.name} {order.user.surname}
+                            </span>
+                        </p>
+                        <p className="text-lg font-bold text-amber-500">
+                            Total a pagar: {""}
+                            <span className="font-normal text-slate-600">{formatCurrency(order.total)}</span>
+                        </p>
+                        <button
+                            type="button"
+                            className="bg-indigo-600 hover:bg-indigo-800 cursor-pointer px-5 py-2 rounded uppercase font-bold text-white text-center w-full"
+                        >
+                            Completar{" "}
+                        </button>
                     </div>
                 ))}
             </div>
