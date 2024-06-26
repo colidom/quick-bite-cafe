2439e6aa31a242aaec2f15927164d17aaf5c496a
colidom <colidom@outlook.com>
2024-06-27 01:18:38 +0200

    Mostrando el Contenido del Pedido

--- app-front/src/views/Orders.jsx
+++ app-front/src/views/Orders.jsx
@@ -12,14 +12,31 @@ export default function Orders() {
 
     const { data, error, isLoading } = useSWR("/api/order", fetcher);
 
-    console.log(data?.data);
-    console.log(error);
-    console.log(isLoading);
+    if (isLoading) return "Cargando pedidos...";
 
     return (
-        <div>
-            <h1 className="text-4xl font-black">Pedidos</h1>
-            <p className="text-2xl my-10">Desde aquí puedes administrar los pedidos.</p>
-        </div>
+        <>
+            <div>
+                <h1 className="text-4xl font-black">Pedidos</h1>
+                <p className="text-2xl my-10">Desde aquí puedes administrar los pedidos.</p>
+            </div>
+            <div>
+                {data.data.data.map((order) => (
+                    <div key={order.id} className="p-5 bg-white shadow space-y-2 border-b">
+                        <p className="text-xl font-bold text-slate-600">Contenido del pedido:</p>
+                        {order.products.map((product) => (
+                            <div key={product.id} className="border-b border-b-slate-200 last-of-type:border-none py-4">
+                                <p className="text-sm">Id producto: {product.id}</p>
+                                <p>{product.name}</p>
+                                <p>
+                                    Cantidad: {""}
+                                    <span className="font-bold">{product.pivot.qty}</span>
+                                </p>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </>
     );
 }
